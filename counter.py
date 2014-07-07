24a37c15047967ffb8f83ad3b3ed6697d13b65d9
The Horus Hawk <brunomadteles@gmail.com>
2014-07-07 20:57:21 +0100

    Added Convidado checkbutton

--- counter.py
+++ counter.py
@@ -2,31 +2,23 @@ from Tkinter import *
 import datetime
 import os.path
 
-nome_do_ficheiro = "contador.csv"
-if not os.path.isfile(nome_do_ficheiro):
+name_of_file = "contador.csv"
+if not os.path.isfile(name_of_file):
 	f = open(nome_do_ficheiro, "w")
 	f.write("Entradas no espaco\nDia do mes, mes, dia da semana, hora\n")
 
-f = open(nome_do_ficheiro, "a")
-print f
+f = open(name_of_file, "a")
 
-class App:
-
-    def __init__(self, master):
-
-        frame = Frame(master)
-        frame.pack()
-
-        self.count_button = Button(frame, text="Clica para gravar entrada", command=self.append_line)
-        self.count_button.pack()
-
-    def append_line(self):
-	line = datetime.datetime.strftime(datetime.datetime.now(), "%d,%b,%A,%H:%M") + "\n"
-        f.write(line)
+def append_line():
+	line = datetime.datetime.strftime(datetime.datetime.now(), "%d,%b,%A,%H:%M") + "," + guest_value.get() + "\n"
+	print guest_value.get
+	f.write(line)
 
 root = Tk()
 
-app = App(root)
+guest_value = StringVar()
+count_button = Button(root, text="Clica para gravar entrada", command=append_line)
+guest = Checkbutton(root, text="Convidado?", variable = guest_value, onvalue="Convidado", offvalue = "Visitante").pack()
+count_button.pack()
 
 root.mainloop()
-# root.destroy()
